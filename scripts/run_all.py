b4c476292400bbab4d6b7ecd607935a6eb5e0dc2
lhiwi <lhiwot181@gmail.com>
2025-06-15 00:58:49 +0300

    Initial commit with project structure and config2

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -1,2 +1,2 @@
 if __name__ == "__main__":
-    print("This is the main runner script.")
+    print("Runner script.")
--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,4 +1,4 @@
 from src.utils import say_hello
 
 def test_say_hello():
-    assert say_hello("Keni") == "Hello, Keni!"
+    assert say_hello("Everyone") == "Hello, Everyone!"
